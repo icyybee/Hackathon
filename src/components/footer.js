4fd7036bd0da9e2faafc9ee44da9cc53f9b59236
Beatrice Egumandi <ushiksmandi@gmail.com>
2023-09-23 00:46:58 +0100

    landing page responsiveness footer completed

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import Logo from '../assets/img/logo.png'
 import { ReactComponent as Phone } from '../assets/svg/Group.svg'
 import { ReactComponent as Map } from '../assets/svg/Vector (7).svg'
@@ -9,20 +10,25 @@ import { ReactComponent as Linkedin } from '../assets/svg/ri_linkedin-fill.svg'
 import Star from '../assets/img/sata gra2.png';
 
 export default function Footer() {
+    const navigate = useNavigate()
+
     return (
-        <div className='section-plain pt-[70px] py-[54px] px-[230px]'>
-            <div className='w-full flex'>
-                <div className='w-[60%]'>
-                    <div className='w-[80%]'>
-                        <img src={Logo} alt='getlinked logo' className='w-[143px] h-[37px] object-contain cursor-pointer' />
-                        <p className='mt-1 text-[12px]'>
+        <div className='section-plain desktop:pt-[70px] pt-[51px] py-[54px] px-[14.19%]'>
+            <div className='w-full flex flex-col desktop:flex-row'>
+                <div className='desktop:w-[60%] w-full'>
+                    <div className='desktop:w-[80%] w-full'>
+                        <h1 onClick={() => navigate('/')} className='font-clash-display cursor-pointer text-[20px] mid:text-[30px] font-bold'>get<span className='text-secondary'>linked</span></h1>
+                        <p className='desktop:mt-1 mt-3 text-[12px]'>
                             Getlinked Tech Hackathon is a technology innovation program 
                             established by a group of organizations with the aim of showcasing 
                             young and talented individuals in the field of technology
                         </p>
                     </div>
                 </div>
-                <div className='flex-1 flex justify-between'>
+                <div className='desktop:hidden mt-[39px] mb-8'>
+                    <p className='flex text-[12px]'>Terms of Use<span className='mx-[10px] w-[2px] h-[17px] bg-secondary inline-block'></span>Privacy Policy</p>
+                </div>
+                <div className='flex-1 flex flex-col desktop:flex-row justify-between'>
                     <div>
                         <h2 className='text-[14px] font-semibold text-secondary mb-[6px]'>Useful Links</h2>
                         <div className='flex flex-col gap-[11px]'>
@@ -32,8 +38,17 @@ export default function Footer() {
                             <p className='text-[12px]'>Register</p>
                         </div>
                     </div>
-                    <div>
-                        <h2 className='text-[14px] font-semibold text-secondary mb-[19px]'>Contact Us</h2>
+                    <div className='flex gap-4 flex-1 items-center desktop:hidden mt-[18px]'>
+                        <p className='text-[12px] text-secondary'>Follow us</p>
+                        <div className='flex gap-[19px] items-center cursor-pointer'>
+                            <Instagram />
+                            <Twitter />
+                            <Facebook />
+                            <Linkedin />
+                        </div>
+                    </div>
+                    <div className='mt-16 desktop:mt-0'>
+                        <h2 className='text-[14px] font-semibold text-secondary desktop:mb-[19px] mb-2'>Contact Us</h2>
                         <div className='flex flex-col gap-[22px]'>
                             <div className='flex gap-4'>
                                 <Phone />
@@ -50,7 +65,7 @@ export default function Footer() {
                     </div>
                 </div>
             </div>
-            <div className='flex mt-[18px]'>
+            <div className='desktop:flex mt-[18px] hidden '>
                 <div className='w-[60%]'>
                     <p className='flex text-[12px]'>Terms of Use<span className='mx-[10px] w-[2px] h-[17px] bg-secondary inline-block'></span>Privacy Policy</p>
                 </div>
@@ -65,7 +80,7 @@ export default function Footer() {
                 </div>
             </div>
             <div className='mt-[67px] w-full flex flex-col items-center'>
-                <img src={Star} alt='star' />
+                <img src={Star} alt='star' className='w-[7px] h-2 desktop:w-auto desktop:h-auto' />
                 <p className='mt-6 text-[12px]'>All rights reserved. © getlinked Ltd.</p>
             </div>
         </div>
--- src/routes/home.js
+++ src/routes/home.js
@@ -41,7 +41,7 @@ export default function Home() {
             <Prizes />  
             <Sponsors />
             <Privacy />
-            {/* <Footer />  */}
+            <Footer /> 
         </motion.div>
     )
 }
