95caee40ea3ff48991f7288f80964a35fe5966b6
Beatrice Egumandi <106768860+icyybee@users.noreply.github.com>
2024-10-28 16:32:46 +0100

    fix: countdown

--- src/components/countdown.js
+++ src/components/countdown.js
@@ -1,28 +1,40 @@
-import React, { useState, useEffect } from 'react'
-import { calculateTimeRemaining } from '../library/helperFunction';
+import React, { useState, useEffect } from "react";
+import { calculateTimeRemaining } from "../library/helperFunction";
 
 export default function Countdown() {
-    const targetDate = new Date('2024-02-01T00:00:00');
-    const [remainingTime, setRemainingTime] = useState(calculateTimeRemaining(targetDate));
+  const targetDate = new Date();
+  targetDate.setDate(targetDate.getDate() + 1);
+  const [remainingTime, setRemainingTime] = useState(
+    calculateTimeRemaining(targetDate)
+  );
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            const newRemainingTime = calculateTimeRemaining(targetDate);
-            setRemainingTime(newRemainingTime);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const newRemainingTime = calculateTimeRemaining(targetDate);
+      setRemainingTime(newRemainingTime);
 
-            if (newRemainingTime.hours === 0 && newRemainingTime.mins === 0 && newRemainingTime.secs === 0) {
-                clearInterval(interval);
-            }
-        }, 1000);
+      if (
+        newRemainingTime.hours === 0 &&
+        newRemainingTime.mins === 0 &&
+        newRemainingTime.secs === 0
+      ) {
+        clearInterval(interval);
+      }
+    }, 1000);
 
-        return () => {
-            clearInterval(interval);
-        };
-    }, []);    
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
-    return (
-        <div className='desktop:text-start text-center desktop:mt-[77px] mt-[14.16px] desktop:mb-[95px] mb-[11.77px] desktop:text-[64px] text-[48px] font-unica-one'>
-            {remainingTime.hours}<span className='text-[14px] font-montserrat'>H</span>   {remainingTime.mins}<span className='text-[14px] font-montserrat'>M</span>   {remainingTime.secs}<span className='text-[14px] font-montserrat'>S</span>
-        </div>
-    )
+  return (
+    <div className="desktop:text-start text-center desktop:mt-[77px] mt-[14.16px] desktop:mb-[95px] mb-[11.77px] desktop:text-[64px] text-[48px] font-unica-one">
+      {remainingTime.hours}
+      <span className="text-[14px] font-montserrat">H</span>{" "}
+      {remainingTime.mins}
+      <span className="text-[14px] font-montserrat">M</span>{" "}
+      {remainingTime.secs}
+      <span className="text-[14px] font-montserrat">S</span>
+    </div>
+  );
 }
